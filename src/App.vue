60b0a9fc76425f4af8f07c6a36ce1f1e3e0d2270
luxinru <865666015@qq.com>
2022-04-18 10:21:56 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,82 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <transition :name="transitionName">
+      <router-view />
+    </transition>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App',
+
+  data () {
+    return {
+      transitionName: 'slide-left'
+    }
+  },
+
+  watch: {
+    // 监听路由切换转场动画
+    $route (to, from) {
+      const toDepth = to.meta.level
+      const fromDepth = from.meta.level
+      this.transitionName = toDepth > fromDepth ? 'slide-left' : 'slide-right'
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.slide-left-enter-active {
+  animation-name: slide-left-in;
+  animation-duration: 0.3s;
+}
+.slide-left-leave-active {
+  animation-name: slide-left-out;
+  animation-duration: 0.3s;
+}
+.slide-right-enter-active {
+  animation-name: slide-right-in;
+  animation-duration: 0.3s;
+}
+.slide-right-leave-active {
+  animation-name: slide-right-out;
+  animation-duration: 0.3s;
 }
 
-#nav {
-  padding: 30px;
+@keyframes slide-left-in {
+  0% {
+    transform: translate3d(100%, 0, 0);
+  }
+  100% {
+    transform: translate3d(0, 0, 0);
+  }
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+@keyframes slide-left-out {
+  0% {
+    transform: translate3d(0, 0, 0);
+  }
+  100% {
+    transform: translate3d(-100%, 0, 0);
+  }
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+@keyframes slide-right-in {
+  0% {
+    transform: translate3d(-100%, 0, 0);
+  }
+  100% {
+    transform: translate3d(0, 0, 0);
+  }
+}
+@keyframes slide-right-out {
+  0% {
+    transform: translate3d(0, 0, 0);
+  }
+  100% {
+    transform: translate3d(100%, 0, 0);
+  }
 }
 </style>
